396a020243229e3a26cc165b22bde64c1f6e3923
05allan1213 <2787554820@qq.com>
2025-04-20 19:56:28 +0800

    增添一些注释

--- include/Poller.h
+++ include/Poller.h
@@ -20,8 +20,11 @@ public:
     virtual ~Poller() = default;
 
     // 给IO复用提供统一的接口
+    // 执行 I/O 复用等待，并将活跃 Channel 填充到 activeChannels
     virtual Timestamp poll(int timeoutMs, ChannelList *activeChannels) = 0;
+    // 添加或更新 Poller 中对某个 Channel (及其 fd) 的监听状态
     virtual void updateChannel(Channel *channel) = 0;
+    // 从 Poller 中移除对某个 Channel (及其 fd) 的监听
     virtual void removeChannel(Channel *channel) = 0;
 
     // 判断参数channel是否在当前Poller中
@@ -33,7 +36,7 @@ protected:
     // map的key: sockfd，value: sockfd所属的channel通道类型
     using ChannelMap = std::unordered_map<int, Channel *>;
 
-    ChannelMap channels_;
+    ChannelMap channels_; // 存储所有注册的Channel
 
 private:
     EventLoop *ownerLoop_; // 定义Poller所属的事件循环EventLoop
--- src/Poller.cpp
+++ src/Poller.cpp
@@ -9,5 +9,6 @@ Poller::Poller(EventLoop *loop)
 bool Poller::hasChannel(Channel *channel) const
 {
     auto it = channels_.find(channel->fd());
+    // 如果找到的记录存在且对应的Channel对象与传入的channel一致，则返回true。
     return it != channels_.end() && it->second == channel;
 }
