4a1502693839120ebe0249972f4d14c37238aa16
05allan1213 <2787554820@qq.com>
2025-04-27 15:10:09 +0800

    增添TcpServer的注释

--- src/TcpServer.cpp
+++ src/TcpServer.cpp
@@ -37,10 +37,11 @@ TcpServer::TcpServer(EventLoop* loop, const InetAddress& listenAddr, const std::
 
 TcpServer::~TcpServer()
 {
+    // 遍历并关闭所有连接
     for (auto& item : connections_)
     {
-        TcpConnectionPtr conn(item.second);
-        item.second.reset();
+        TcpConnectionPtr conn(item.second);  // 获取连接的shared_ptr
+        item.second.reset();  // 置空shared_ptr，断开TcpServer对TcpConnection对象的强引用
         conn->getLoop()->runInLoop(std::bind(&TcpConnection::connectDestroyed, conn));
     }
 }
@@ -61,6 +62,8 @@ void TcpServer::newConnection(int sockfd, const InetAddress& peerAddr)
 {
     // 轮询获取一个subLoop，以管理channel
     EventLoop* ioLoop = threadPool_->getNextLoop();
+
+    // 生成连接名称
     char buf[64] = {0};
     snprintf(buf, sizeof(buf), "-%s#%d", ipPort_.c_str(), nextConnId_);
     ++nextConnId_;
@@ -69,6 +72,7 @@ void TcpServer::newConnection(int sockfd, const InetAddress& peerAddr)
     LOG_INFO("TcpServer::newConnection [%s] - new connection [%s] from %s\n", name_.c_str(),
              connName.c_str(), peerAddr.toIpPort().c_str());
 
+    // 获取本地地址
     sockaddr_in local;
     ::bzero(&local, sizeof local);
     socklen_t addrlen = sizeof local;
@@ -78,12 +82,18 @@ void TcpServer::newConnection(int sockfd, const InetAddress& peerAddr)
     }
     InetAddress localAddr(local);
 
+    // 创建TcpConnection对象
     TcpConnectionPtr conn(new TcpConnection(ioLoop, connName, sockfd, localAddr, peerAddr));
+    // 存储新连接
     connections_[connName] = conn;
+    // 设置TcpConnection回调
     conn->setConnectionCallback(connectionCallback_);
     conn->setMessageCallback(messageCallback_);
     conn->setWriteCompleteCallback(writeCompleteCallback_);
+    // 设置内部关闭回调
     conn->setCloseCallback(std::bind(&TcpServer::removeConnection, this, std::placeholders::_1));
+
+    // 启动TCPConnection
     ioLoop->runInLoop(std::bind(&TcpConnection::connectEstablished, conn));
 }
 
@@ -93,11 +103,14 @@ void TcpServer::removeConnection(const TcpConnectionPtr& conn)
 }
 
 void TcpServer::removeConnectionInLoop(const TcpConnectionPtr& conn)
+// 此方法总是在TCPServer所属的 mainLoop 线程中执行
 {
     LOG_INFO("TcpServer::removeConnectionInLoop [%s] - connection %s\n", name_.c_str(),
              conn->name().c_str());
-
+    // 从map中删除
     connections_.erase(conn->name());
+    // 获取连接所属的subLoop
     EventLoop* ioLoop = conn->getLoop();
+    // 删除连接
     ioLoop->queueInLoop(std::bind(&TcpConnection::connectDestroyed, conn));
 }
